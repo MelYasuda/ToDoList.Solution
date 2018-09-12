5a5b5e131df276d51e3d1b79792e7c944950f82b
Chris Crow and Mel Yasuda <chris@>
2018-09-12 08:58:33 -0700

    add id to each item

--- ToDoList/Controllers/ItemsControllers.cs
+++ ToDoList/Controllers/ItemsControllers.cs
@@ -33,5 +33,12 @@ namespace ToDoList.Controllers
             return View("Index", tempList);
         }
 
+        [HttpGet("/items/{id}")]
+        public ActionResult Details(int id)
+        {
+          Item item = Item.Find(id);
+          return View(item);
+        }
+
     }
 }
